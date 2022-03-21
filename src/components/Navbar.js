3de822d431d50a8957eb0df5075040b29fc00e2e
Gagandeep4656 <gd4656@gmail.com>
2022-03-21 23:54:10 +0530

    color palette

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,14 +20,14 @@ export default function Navbar(props) {
         </li>
         
       </ul>
-      {/* <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form> */}
-      <div className={`form-check form-switch text-${props.mode==='light'?'dark': 'light'}`} >
-        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
-                </div>
+     <div className="d-flex"></div>
+     <div className="bg-primary rounded mx-2" onClick={()=>props.toggleMode('primary')} style={{height: '25px', width: '25px', cursor: 'pointer'}} ></div>
+     <div className="bg-danger rounded mx-2" onClick={()=>props.toggleMode('danger')} style={{height: '25px', width: '25px', cursor: 'pointer'}} ></div>
+     <div className="bg-success rounded mx-2" onClick={()=>props.toggleMode('success')} style={{height: '25px', width: '25px', cursor: 'pointer'}} ></div>
+     <div className="bg-warning rounded mx-2" onClick={()=>props.toggleMode('warning')} style={{height: '25px', width: '25px', cursor: 'pointer'}} ></div>
+     <div className="bg-light rounded mx-2" onClick={()=>props.toggleMode('light')} style={{height: '25px', width: '25px', border:'2px solid black' ,cursor: 'pointer'}} ></div>
+     <div className="bg-dark rounded mx-2" onClick={()=>props.toggleMode('dark')} style={{height: '25px', width: '25px', cursor: 'pointer'}} ></div>
+     
     </div>
   </div>
 </nav>
